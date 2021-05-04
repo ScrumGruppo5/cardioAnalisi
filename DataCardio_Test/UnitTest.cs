a4e5b9014230f778f48e199d9defcf4505e78b80
_ Trentini _ <pietrotrentini5@gmail.com>
2021-05-04 13:03:25 +0200

    modifica

--- DataCardio_Test/UnitTest.cs
+++ DataCardio_Test/UnitTest.cs
@@ -7,22 +7,39 @@ namespace DataCardio_Test
     public class UnitTest
     {
 
-            [TestMethod]
-            public void TestMethod1()
-            {
-                int età = 20;
-                double valoreaspettato = 200;
-                double prova = DataCardio.Frequenzabattito(età);
-                Assert.AreEqual(valoreaspettato, prova);
-            }
-            [TestMethod]
-            public void TestMethod2()
-            {
-                int f = 200;
-                bool valoreaspettato = false;
-                bool prova = DataCardio.FrequenzaConsigliata(f);
-                Assert.AreEqual(valoreaspettato, prova);
-            }
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int età = 20;
+            double valoreaspettato = 200;
+            double prova = DataCardio.Frequenzabattito(età);
+            Assert.AreEqual(valoreaspettato, prova);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int f = 200;
+            bool valoreaspettato = false;
+            bool prova = DataCardio.FrequenzaConsigliata(f);
+            Assert.AreEqual(valoreaspettato, prova);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            int battiti = 60;
+            string valoreAspettato = "Normale";
+            string prova = DataCardio.FrequenzaCardiaca(battiti);
+            Assert.AreEqual(valoreAspettato, prova);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            int età = 20;
+            string sesso = "Uomo";
+            double peso = 70, durata = 10, frequenza = 200;
+            double valoreAspettato = 0;
+            double prova = DataCardio.CalorieBruciate(sesso, età, peso, durata, frequenza);
+            Assert.AreEqual(valoreAspettato, prova);
         }
     }
 }
--- cardioLibrary/DataCardio.cs
+++ cardioLibrary/DataCardio.cs
@@ -19,5 +19,37 @@ namespace cardioLibrary
             else
                 return false;
         }
+        public static string FrequenzaCardiaca(int battiti)
+        {
+            if (battiti <= 0)
+                return "Valore errato";
+            {
+                if (battiti < 60)
+                    return "Bradicardia";
+                else if (battiti >= 60 && battiti <= 100)
+                    return "Normale";
+                else
+                    return "Tachicardia";
+            }
+        }
+        public static double CalorieBruciate(string sesso, int età, double frequenza, double peso, double durata)
+        {
+            double c;
+            if (sesso == "uomo" || sesso == "Uomo")
+            {
+                c = ((età * 0.2017) + (peso * 0.199) + (frequenza * 0.6309) - 55.0969) * durata / 4.184;
+            }
+            else if (sesso == "Donna" || sesso == "donna")
+            {
+                c = ((età * 0.074) - (peso * 0.126) + (frequenza * 0.4472) - 20.4022) * durata / 4.184;
+            }
+            else
+                return 1;
+            return c;
+
+
+
+
+        }
     }
 }
